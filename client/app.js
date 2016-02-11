1e89de1568efdda2f7ba8b00e257d7fa258cfdbe
Fanneyyy <fanneyyyy@gmail.com>
2016-02-11 23:28:22 +0000

    Heroku stuff

--- client/app.js
+++ client/app.js
@@ -4,7 +4,7 @@ angular.module('chat').controller('homecontroller',function($scope){
     $scope.username = '';
 
     $scope.login = function(){
-        var socket = io.connect('http://localhost:8080');
+        var socket = io.connect('https://blooming-reaches-41821.herokuapp.com/');
         socket.emit("adduser", $scope.username, function(available){                    
             $scope.available = available;
             $scope.$apply();
