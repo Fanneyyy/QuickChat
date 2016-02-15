768f5e85bb325a0e0c609039f2329c2574afd5b6
Fanneyyy <fanneyyyy@gmail.com>
2016-02-15 15:16:06 +0000

    Heroku stuff

--- client/app.js
+++ client/app.js
@@ -4,7 +4,7 @@ angular.module('quickchat').controller('homecontroller',function($scope){
     $scope.username = '';
 
     $scope.login = function(){
-        var socket = io.connect('//fanneyyy-quickchat.herokuapp.com');
+        var socket = io.connect();
         socket.emit("adduser", $scope.username, function(available){                    
             $scope.available = available;
             $scope.$apply();
