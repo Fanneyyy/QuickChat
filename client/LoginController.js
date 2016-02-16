87a982449fc397fc78056dba19f2ab9dc7a05be4
Kristinn Þorri <kristinn.thorri@gmail.com>
2016-02-16 11:46:42 +0000

    fixing controller issues

---
+++ client/LoginController.js
@@ -0,0 +1,26 @@
+
+angular.module("quickchat").controller("LoginController", 
+        ["$scope", "$http", "socket", function($scope, $http, socket) {
+    $scope.nick = "";
+    $scope.loggedIn = false;
+    $scope.rooms = [];
+    $scope.users = [];
+
+    socket.on("roomlist", function(data) {
+        $scope.rooms = data;
+    });
+
+    socket.on("userlist", function(data) {
+        $scope.users = data;
+    });
+
+    $scope.login = function login() {
+        socket.emit("adduser", $scope.nick, function(available) {
+            if (available){
+                $scope.loggedIn = true;
+                socket.emit("rooms");
+                socket.emit("users");
+            }
+        });
+    };
+}]);
--- client/app.js
+++ client/app.js
@@ -1,4 +1,9 @@
 "use strict";
+angular.module("quickchat", [
+    'ngRoute',
+    'LoginController'
+    ]);
+
 
 angular.module("quickchat", ["ngRoute"]).config(function($routeProvider) {
     $routeProvider
@@ -22,34 +27,10 @@ angular.module("quickchat", ["ngRoute"]).config(function($routeProvider) {
         redirectTo: "/home/index"
     });
 });
-angular.module("quickchat", []);
+
 
 angular.module("quickchat").controller("HomeController", 
         ["$scope", "$http", "socket", function($scope, $http, socket) {
 
-    $scope.nick = "";
-    $scope.loggedIn = false;
-    $scope.rooms = [];
-    $scope.users = [];
 
-    socket.on("roomlist", function(data) {
-        $scope.rooms = data;
-    });
-
-    socket.on("userlist", function(data) {
-        $scope.users = data;
-    });
-
-    $scope.login = function login() {
-        socket.emit("adduser", $scope.nick, function(available) {
-            if (available){
-                $scope.loggedIn = true;
-                socket.emit("rooms");
-                socket.emit("users");
-            }
-        });
-    };
-    $scope.joinRoom = function joinRoom() {
-        socekt.emit("joinroom", $scope.name, )
-    }
 }]);
